c51dcbf60bdac236b604aedde4fab6ba900cc24b
Otávio <otavioaugusto42@hotmail.com>
2024-11-01 18:04:18 -0300

    Adicionando CORS para solução de problemas no Frontend

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { Request, Response } from 'express';
 import routerProdutos from './routes/products';
+import cors from 'cors';
 import routerUsuarios from './routes/users';
 
 const app = express();
 const port = 5000; 
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
 app.use('/api', routerProdutos);
 app.use('/api', routerUsuarios);
+
 
 app.get('/', (req: Request, res: Response) => { 
     res.send('Hello Mind Group!'); 
